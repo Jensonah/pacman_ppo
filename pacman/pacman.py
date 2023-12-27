f50a0098e84cd2fde1d8d3ba908c6f30ef045175
Jensonah <jenshartsuiker@gmail.com>
2023-12-27 15:29:18 +0100

    Made first steps adapting ppo code to pacman, including model architectures

--- pacman/pacman.py
+++ pacman/pacman.py
@@ -1,11 +1,355 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+import numpy as np
+import matplotlib.pyplot as plt
 import gymnasium as gym
-env = gym.make("ALE/Pacman-v5", render_mode="human")
-observation, info = env.reset(seed=42)
-for _ in range(1000):
-   action = env.action_space.sample()  # this is where you would insert your policy
-   observation, reward, terminated, truncated, info = env.step(action)
+from tqdm import tqdm
+from pathlib import Path
 
-   if terminated or truncated:
-      observation, info = env.reset()
+
+class Actor(nn.Module):
+
+	def __init__(self, device):
+		super(Actor, self).__init__()
+
+		# (250,160,3)
+		self.conv1 = nn.Conv2d(3, 6, (3,3), groups=1, stride=(2,1))
+		# (125, 160, 6)
+		self.conv2 = nn.Conv2d(6, 6, (3,3), groups=1, stride=(2,2))
+		# (62, 80, 6)
+		self.conv3 = nn.Conv2d(6, 6, (3,3), groups=1, stride=(2,2))
+		# (31, 40, 6)
+		self.conv4 = nn.Conv2d(6, 6, (3,3), groups=1, stride=(2,2))
+		# (15, 20, 6)
+		self.conv5 = nn.Conv2d(6, 6, (3,3), groups=1, stride=(2,2))
+		# (7, 10, 6)
+		self.flat = nn.Flatten()
+		# 7*10*6 = 420
+		self.full = nn.Linear(7*10*6, 5)
+		# 5
+
+		self.device = device
+		
+
+	def forward(self, x):
+
+		x = self.conv1(x)
+		x = F.relu(x)
+
+		x = self.conv2(x)
+		x = F.relu(x)
+
+		x = self.conv3(x)
+		x = F.relu(x)
+
+		x = self.conv4(x)
+		x = F.relu(x)
+
+		x = self.conv5(x)
+		x = F.relu(x)
+		
+		x = self.flat(x)
+
+		x = self.full(x)
+		x = F.softmax(x, dim=1)
+
+		return x
+	
+
+	def act(self, state):
+
+		probabilities = self.forward(state)
+		probs = Categorical(probabilities)
+		action = probs.sample()
+		# PPO paper says to use normal prob here
+		return action.item(), probs.log_prob(action).exp()
+	
+
+class Critic(nn.Module):
+
+	def __init__(self, device):
+		super(Critic, self).__init__()		
+
+		# (250,160,3)
+		self.conv1 = nn.Conv2d(3, 6, (3,3), groups=1, stride=(2,1))
+		# (125, 160, 6)
+		self.conv2 = nn.Conv2d(6, 6, (3,3), groups=1, stride=(2,2))
+		# (62, 80, 6)
+		self.conv3 = nn.Conv2d(6, 6, (3,3), groups=1, stride=(2,2))
+		# (31, 40, 6)
+		self.conv4 = nn.Conv2d(6, 6, (3,3), groups=1, stride=(2,2))
+		# (15, 20, 6)
+		self.conv5 = nn.Conv2d(6, 6, (3,3), groups=1, stride=(2,2))
+		# (7, 10, 6)
+		self.flat = nn.Flatten()
+		# 7*10*6 = 420
+		self.full = nn.Linear(7*10*6, 1)
+		# 1
+
+		self.device = device
+		
+
+	def forward(self, x):
+
+		x = self.conv1(x)
+		x = F.relu(x)
+
+		x = self.conv2(x)
+		x = F.relu(x)
+
+		x = self.conv3(x)
+		x = F.relu(x)
+
+		x = self.conv4(x)
+		x = F.relu(x)
+
+		x = self.conv5(x)
+		x = F.relu(x)
+		
+		x = self.flat(x)
+
+		x = self.full(x)
+		x = F.sigmoid(x, dim=1)
+
+		return x
+
+
+def prepare_data_for_model(state, device):
+
+	state = np.array(state) / 255
+
+	# PyTorch wants the rgb channel first
+	transposed_array = np.transpose(state, (2, 0, 1))
+
+	tensor = torch.from_numpy(transposed_array).float().unsqueeze(0).to(device)
+
+	return tensor
+
+
+def collect_episode(env, model):
+
+	episode = []
+	terminated = False
+	truncated = False
+
+	new_state, info = env.reset()
+	
+	while not (terminated or truncated):
+
+		state = prepare_data_for_model(new_state, actor.device)
+
+		action, probs = model.act(state)
+
+		new_state, reward, terminated, truncated, info = env.step(action)
+
+		reward += info['lives']
+
+		episode.append((state, action, probs, reward))
+
+	return episode
+
+
+# index probability of action taken at sampling time with current updated model
+def get_probs(actor, states, actions):
+	all_probs = torch.cat([actor.forward(state) for state in states])
+	actions = torch.Tensor(actions).int()
+	out = all_probs[torch.arange(all_probs.size(0)), actions]
+	# these lines do this below more efficient
+	#[all_probs[i][actions[i]].unsqueeze(0) for i in range(len(actions))]
+	return out
+
+
+def update_net(gradients, optim):
+	
+	policy_loss = gradients.sum()
+	optim.zero_grad()
+	policy_loss.backward()
+	optim.step()
+
+	return policy_loss
+
+
+def get_standardized_tensor(xs):
+
+	## eps is the smallest representable float, which is 
+	# added to the standard deviation of the returns to avoid numerical instabilities   
+	eps = np.finfo(np.float32).eps.item()     
+	xs = torch.tensor(xs)
+	return (xs - xs.mean()) / (xs.std() + eps)
+
+
+def train(env, actor, critic, optim, num_episodes, num_actors, num_epochs, eps, gamma):
+
+	# One style of policy gradient implementation, popularized in [Mni+16] and well-suited for use
+	# with recurrent neural networks, runs the policy for T timesteps (where T is much less than the
+	# episode length), and uses the collected samples for an update. This style requires an advantage
+	# estimator that does not look beyond timestep T .
+	# Don't we already have this? Think about this. Or isn't that the entire idea of the estimator?
+
+	obj_func_hist = []
+	losses = []
+
+	for _ in tqdm(range(num_episodes)):
+
+		episodes = [collect_episode(env, actor) for _ in range(num_actors)]
+
+		sum_tuples = 0
+		for episode in episodes:
+			sum_tuples += len(episode)
+
+		'''
+		In other implementations the data is not a collection of varying length concluded episodes,
+		but instead an array of i*j, where j is a fixed number of steps.
+		So a row could be a partial episode (most likely), or multiple entire episodes, 
+		with possibly the last one being truncated.
+		'''
+
+		states = [[state for state, _, _, _ in episode] for episode in episodes]
+		actions = [[action for _, action, _, _ in episode] for episode in episodes]
+		original_probs  = [torch.cat([prob for _, _, prob, _ in episode]) for episode in episodes]
+		rewards = [[reward for _, _, _, reward in episode] for episode in episodes]
+
+		cum_rewards = np.array([sum(episode) for episode in rewards])
+		obj_func_hist.append((cum_rewards.mean(), cum_rewards.min(), cum_rewards.max()))
+
+		rewards = [get_standardized_tensor(reward).unsqueeze(1) for reward in rewards]
+
+		for k in range(num_epochs):
+
+			actor_gradient = torch.empty(0)
+			critic_gradient = torch.empty(0)
+
+			for j in range(num_actors):
+
+				critic_values_t  = torch.cat([critic(state) for state in states[j]])
+				critic_values_t1 = torch.cat((critic_values_t.clone()[1:], torch.zeros(1,1)))
+
+				advantage = gamma*critic_values_t1 + rewards[j] - critic_values_t
+
+				# we need the probability for each action
+				if k == 0:
+					actor_probs = original_probs[j].clone()
+					original_probs[j] = original_probs[j].detach()
+				else:
+					# we can also always do this and detach already all above
+					actor_probs = get_probs(actor, states[j], actions[j])
+
+				difference_grad = torch.div(actor_probs, original_probs[j]).unsqueeze(1)
+				# Note that for k = 0 these are all ones, but we keep the calculation such that the
+				# backtracking algorithm can also see this
+				
+				clipped = torch.clamp(difference_grad, 1 - eps, 1 + eps)
+
+				ppo_gradient = torch.minimum(difference_grad*advantage, clipped*advantage)
+				ppo_gradient *= -1 # this seems to be the right place
+
+				actor_gradient = torch.cat((actor_gradient, ppo_gradient))
+				# we could also include an "entropy" bonus to the actor loss that encourages exploration
+				
+				critic_gradient = torch.cat((critic_gradient, advantage*advantage))
+
+			# update both models
+			gradient = torch.cat((actor_gradient, critic_gradient))
+			loss = update_net(gradient, optim)
+			losses.append(loss.detach())
+
+	return obj_func_hist, losses
+
+
+def plot_fancy_loss(data, path):
+
+	means, mins, maxes = list(map(list, zip(*data)))
+
+	plt.clf()
+
+	xs = range(len(data))
+
+	# Plot points
+	plt.plot(xs, means, 'b', alpha=0.8)
+	#plt.plot(pfb_x_mean, pfb_y_mean, 'g', alpha=0.8)
+
+	# Plot errors
+	plt.fill_between(xs, mins, maxes, color='b', alpha=0.3)
+	#plt.fill_between(pfb_x_mean, pfb_y_low, pfb_y_high, color='g', alpha=0.3)
+
+	# Set labels
+	plt.title(f'Reward projection')
+	plt.xlabel('Episode No.')
+	plt.ylabel(f'Reward')
+	plt.legend(['My PPO implementation'])#, 'PPO for Beginners'])\
+	plt.savefig(path)
+
+
+def plot_ugly_loss(data, length, name):
+
+	plt.clf()
+	plt.plot(list(range(length)), data)
+	plt.savefig(f"{base_path}/{name}loss.png")
+
+
+pacman_hyperparameters = {
+	"num_episodes" : 10,
+	"gamma" : 0.99,
+	"lr" : 1e-3,
+	"env_name" : "ALE/Pacman-v5",
+	"render_mode" : "rgb_array",
+	"trial_number" : 1,
+	"eps" : 0.2,
+	"num_epochs" : 1,
+	"num_actors" : 1,
+	"device" : "cpu"
+}
+
+# input (250, 160, 3)
+# reward float, increases when pacman eats. Further information about reward func unknown
+# info contains lives, dict object
+
+env = gym.make(pacman_hyperparameters["env_name"], 
+			   render_mode=pacman_hyperparameters["render_mode"]
+			   )
+
+base_path = f"trial_data/trial_{pacman_hyperparameters['trial_number']}"
+
+env = gym.wrappers.RecordVideo(env, f"{base_path}/video/", episode_trigger=lambda t: t % 100 == 99)
+
+actor = Actor(pacman_hyperparameters["device"])
+
+critic = Critic(pacman_hyperparameters["device"])
+
+optimizer = optim.Adam(list(actor.parameters()) + list(critic.parameters()), lr=pacman_hyperparameters["lr"])
+
+obj_func_hist, losses = train(env,
+							  actor,
+							  critic,
+							  optimizer,
+							  pacman_hyperparameters["num_episodes"],
+							  pacman_hyperparameters["num_actors"],
+							  pacman_hyperparameters["num_epochs"],
+							  pacman_hyperparameters["eps"],
+							  pacman_hyperparameters["gamma"])
+
+
+with open(f'{base_path}/hyperparameters.txt', 'w') as f:
+    f.write(str(pacman_hyperparameters))
+
+# check if directory exist, if not, make it
+Path(f"{base_path}/save/").mkdir(parents=True, exist_ok=True)
+
+plot_fancy_loss(obj_func_hist,
+				f"{base_path}/rewards.png")
+
+plot_ugly_loss(losses,
+			   len(losses),
+			   "total_")
+
+torch.save(actor.state_dict(), f"{base_path}/save/actor_weights.pt")
+torch.save(critic.state_dict(), f"{base_path}/save/critic_weights.pt")
 
 env.close()
+
+# We found that using a gpu does not really speed up our code
+# However we did not optimize our code to do so either
+# We could change our code to train on small batches etc.
